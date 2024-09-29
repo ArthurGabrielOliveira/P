e35017eaea5da17d2454334bf118f560d7886cae
ArthurGabrielOliveira <oliveira.arthurgabiel@gmail.com>
2024-09-29 09:05:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,5 +7,9 @@ let chute = prompt ("escolha um numero de 1 a 30");
 if (chute == numeroSecreto){
     alert(`voce acertou o numero secreto ${numeroSecreto}`);
 } else {
-    alert ('Errou!!')
-}
+    if (chute > numeroSecreto){
+        alert (`O numero secreto é menor que ${chute}`);
+    } else{
+        alert (`O numero secreto é maior que ${chute}`)
+    }
+} 
