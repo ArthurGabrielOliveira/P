51d15731079d7b76f75ad4bb58f86ea36b9d36fc
ArthurGabrielOliveira <oliveira.arthurgabiel@gmail.com>
2024-09-30 15:49:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-alert ("bem vindo ao jogo");
-let numeroSecreto = 5;
-console.log(numeroSecreto);
-let chute; 
+alert('bem vindo ao jogo');
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let chute;
+console.log(numeroSecreto); 
 let tentativas = 1;
  
 // while é usado como enquanto
 while (chute != numeroSecreto) {
-    chute = prompt ("escolha um numero de 1 a 30");
+    chute = prompt('escolha um numero de 1 a 100');
 // se o chute for igual ao numero secreto
-    if (chute == numeroSecreto){
-        alert(`voce acertou o numero secreto ${numeroSecreto} com ${tentativas}`);
+    if (chute == numeroSecreto) {
+        break;
     } else {
         if (chute > numeroSecreto){
-            alert (`O numero secreto é menor que ${chute}`);
-        } else{
-            alert (`O numero secreto é maior que ${chute}`);
+            alert(`O numero secreto é menor que ${chute}`);
+        } else {
+            alert(`O numero secreto é maior que ${chute}`);
         }
         tentativas++;
     } 
-} 
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`voce acertou o numero secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+//    if (tentativas > 1){
+  //      alert(`voce acertou o numero secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    //} else {
+      //  alert(`voce acertou o numero secreto ${numeroSecreto} com ${tentativas} tentativa`);
+    // } 
