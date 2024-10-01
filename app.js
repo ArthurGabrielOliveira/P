0addc8cc428110fc6beaa4e7f38aa03c5dd9920f
ArthurGabrielOliveira <oliveira.arthurgabiel@gmail.com>
2024-10-01 12:28:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 alert('bem vindo ao jogo');
 let numeroMaximo = 5000;
-let numeroSecreto = parseInt(Math.random() * numeroMaximo);
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 let chute;
 console.log(numeroSecreto); 
 let tentativas = 1;
  
 // while é usado como enquanto
 while (chute != numeroSecreto) {
-    chute = prompt(`escolha um numero de 1 a ${numeroMaximo}`);
+    chute = prompt(`Escolha um numero entre 1 a ${numeroMaximo}`);
 // se o chute for igual ao numero secreto
     if (chute == numeroSecreto) {
         break;
@@ -28,7 +28,7 @@ alert(`voce acertou o numero secreto ${numeroSecreto} com ${tentativas} ${palavr
   //      alert(`voce acertou o numero secreto ${numeroSecreto} com ${tentativas} tentativas`);
     //} else {
       //  alert(`voce acertou o numero secreto ${numeroSecreto} com ${tentativas} tentativa`);
-    // } 1
+    // } 
 
 
 
